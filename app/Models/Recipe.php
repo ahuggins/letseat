f39669d7e765a2dd1498e684f5313d0b4834706c
Andy Huggins <andrewhuggins@gmail.com>
2026-02-22 14:45:48 -0500

    add toSearchablearray on Recipe

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -7,14 +7,23 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Laravel\Scout\Searchable;
 
 class Recipe extends Model
 {
-    use HasComments, HasFactory;
+    use HasComments, HasFactory, Searchable;
 
     protected $guarded = [];
 
     protected $with = ['user', 'comments', 'comments.commentator'];
+
+    public function toSearchableArray()
+    {
+        return array_merge(['name' => $this->name, 'ingredients' => $this->ingredients], [
+            'id' => (string) $this->id,
+            'created_at' => $this->created_at->timestamp,
+        ]);
+    }
 
     public function user()
     {
